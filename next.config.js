f82198e332c117ddf779af10544bbafc948b6abd
Sushmi1705 <sushmiwilliam@gmail.com>
2025-07-07 21:31:46 +0530

    Add export script for static deployment

--- next.config.js
+++ next.config.js
@@ -2,9 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export', // ✅ THIS is required for static export
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
